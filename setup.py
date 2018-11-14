85c7891688666fc8a0a5ae5ad453d249c52353d4
BenjaminRueth <benjamin.rueth@tum.de>
2018-11-14 16:42:16 +0100

    made python3 support explicit

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+# from https://stackoverflow.com/a/9079062
+import sys  
+if sys.version_info[0] < 3:
+    raise Exception("The fenics-adapter only supports Python3. Did you run $python setup.py <option>.? Try running $python3 setup.py <option>.")
+
 setup(name='fenics-adapter',
       version='0.1',
       description='preCICE-adapter for the open source computing platform FEniCS ',
