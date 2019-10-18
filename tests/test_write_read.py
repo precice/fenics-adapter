df580c0c66cf22dcff71342652bf528cb522c0d1
BenjaminRueth <benjamin.rueth@tum.de>
2019-10-18 14:17:13 +0200

    fix test_write_read

--- tests/test_write_read.py
+++ tests/test_write_read.py
@@ -22,6 +22,7 @@ class RightBoundary(SubDomain):
 class TestWriteData(TestCase):
     dummy_config = "tests/precice-adapter-config-WR1.json"
     n_vertices = 11
+    dimensions = 2
     n_cells = n_vertices - 1
     mesh = UnitSquareMesh(n_cells, n_cells)
     dummy_vertex_ids = np.arange(n_vertices)
@@ -45,8 +46,8 @@ class TestWriteData(TestCase):
         import fenicsadapter
         Interface.configure = MagicMock()
         Interface.write_block_scalar_data = MagicMock()
-        Interface.read_block_vector_data = MagicMock()
-        Interface.get_dimensions = MagicMock(return_value=2)
+        Interface.read_block_vector_data = MagicMock(return_value=np.random.rand(self.n_vertices, self.dimensions))
+        Interface.get_dimensions = MagicMock(return_value=self.dimensions)
         Interface.set_mesh_vertices = MagicMock(return_value=self.dummy_vertex_ids)
         Interface.initialize = MagicMock(return_value=self.precice_dt)
         Interface.initialize_data = MagicMock()
@@ -69,25 +70,20 @@ class TestWriteData(TestCase):
         precice.advance(write_u, u_init, u_init, 0, self.fenics_dt, 0)
 
         expected_data_id = 15
-        expected_size = 11
         expected_values = np.array([self.scalar_expr(x_right, y) for y in np.linspace(y_bottom, y_top, 11)])
-        expected_ids = np.zeros(11)
+        expected_ids = self.dummy_vertex_ids
 
-        expected_args = [expected_data_id, expected_size, expected_ids, expected_values]
-
-        for arg, expected_arg in zip(Interface.write_block_scalar_data.call_args[0], expected_args):
-            if type(arg) is int:
-                self.assertTrue(arg == expected_arg)
-            elif type(arg) is np.ndarray:
-                np.testing.assert_allclose(arg, expected_arg)
+        self.assertTrue(expected_data_id == Interface.write_block_scalar_data.call_args[0][0])
+        np.testing.assert_allclose(expected_ids, Interface.write_block_scalar_data.call_args[0][1])
+        np.testing.assert_allclose(expected_values, Interface.write_block_scalar_data.call_args[0][2])
 
     def test_write_vector_data(self):
         from precice_future import Interface
         import fenicsadapter
         Interface.configure = MagicMock()
         Interface.write_block_vector_data = MagicMock()
-        Interface.read_block_scalar_data = MagicMock()
-        Interface.get_dimensions = MagicMock(return_value=2)
+        Interface.read_block_scalar_data = MagicMock(return_value=np.random.rand(self.n_vertices))
+        Interface.get_dimensions = MagicMock(return_value=self.dimensions)
         Interface.set_mesh_vertices = MagicMock(return_value=self.dummy_vertex_ids)
         Interface.initialize = MagicMock(return_value=self.precice_dt)
         Interface.initialize_data = MagicMock()
@@ -110,32 +106,25 @@ class TestWriteData(TestCase):
         precice.advance(write_u, u_init, u_init, 0, self.fenics_dt, 0)
 
         expected_data_id = 15
-        expected_size = self.n_vertices
-        expected_values_x = np.array([self.vector_expr(x_right, y)[0] for y in np.linspace(y_bottom, y_top, 11)])
-        expected_values_y = np.array([self.vector_expr(x_right, y)[1] for y in np.linspace(y_bottom, y_top, 11)])
-        expected_values = np.stack([expected_values_x, expected_values_y]).T.ravel()
-        expected_ids = np.zeros(11)
+        expected_values_x = np.array([write_u(x_right, y)[0] for y in np.linspace(y_bottom, y_top, 11)])
+        expected_values_y = np.array([write_u(x_right, y)[1] for y in np.linspace(y_bottom, y_top, 11)])
+        expected_values = np.column_stack([expected_values_x, expected_values_y])
+        expected_ids = self.dummy_vertex_ids
 
-        expected_args = [expected_data_id, expected_size, expected_ids, expected_values]
-
-        for arg, expected_arg in zip(Interface.write_block_vector_data.call_args[0], expected_args):
-            if type(arg) is int:
-                self.assertTrue(arg == expected_arg)
-            elif type(arg) is np.ndarray:
-                np.testing.assert_almost_equal(arg, expected_arg)
+        self.assertTrue(expected_data_id == Interface.write_block_vector_data.call_args[0][0])
+        np.testing.assert_allclose(expected_ids, Interface.write_block_vector_data.call_args[0][1])
+        np.testing.assert_allclose(expected_values, Interface.write_block_vector_data.call_args[0][2])
 
     def test_read_scalar_data(self):
         from precice_future import Interface
         import fenicsadapter
 
-        def return_dummy_data(unused1, unused2, unused3, read_data):
-            for i in range(len(read_data)):
-                read_data[i] = i
+        return_dummy_data = np.arange(self.n_vertices)
 
         Interface.configure = MagicMock()
         Interface.write_block_vector_data = MagicMock()
-        Interface.read_block_scalar_data = MagicMock(side_effect=return_dummy_data)
-        Interface.get_dimensions = MagicMock(return_value=2)
+        Interface.read_block_scalar_data = MagicMock(return_value=return_dummy_data)
+        Interface.get_dimensions = MagicMock(return_value=self.dimensions)
         Interface.set_mesh_vertices = MagicMock(return_value=self.dummy_vertex_ids)
         Interface.initialize = MagicMock(return_value=self.precice_dt)
         Interface.initialize_data = MagicMock()
@@ -158,30 +147,22 @@ class TestWriteData(TestCase):
         precice.advance(write_u, u_init, u_init, 0, self.fenics_dt, 0)
 
         expected_data_id = 15
-        expected_size = 11
-        expected_values = np.array([i for i in range(11)])
-        expected_ids = np.zeros(11)
+        expected_ids = self.dummy_vertex_ids
 
-        expected_args = [expected_data_id, expected_size, expected_ids, expected_values]
-
-        for arg, expected_arg in zip(Interface.read_block_scalar_data.call_args[0], expected_args):
-            if type(arg) is int:
-                self.assertTrue(arg == expected_arg)
-            elif type(arg) is np.ndarray:
-                np.testing.assert_allclose(arg, expected_arg)
+        self.assertTrue(expected_data_id == Interface.read_block_scalar_data.call_args[0][0])
+        np.testing.assert_allclose(expected_ids, Interface.read_block_scalar_data.call_args[0][1])
 
     def test_read_vector_data(self):
         from precice_future import Interface
         import fenicsadapter
 
-        def return_dummy_data(unused1, unused2, unused3, read_data):
-            for i in range(len(read_data)):
-                read_data[i] = i
+        return_dummy_data = np.random.rand(self.n_vertices, self.dimensions)
 
         Interface.configure = MagicMock()
         Interface.write_block_scalar_data = MagicMock()
-        Interface.read_block_vector_data = MagicMock(side_effect=return_dummy_data)
-        Interface.get_dimensions = MagicMock(return_value=2)
+        Interface.read_block_scalar_data = MagicMock()
+        Interface.read_block_vector_data = MagicMock(return_value=return_dummy_data)
+        Interface.get_dimensions = MagicMock(return_value=self.dimensions)
         Interface.set_mesh_vertices = MagicMock(return_value=self.dummy_vertex_ids)
         Interface.initialize = MagicMock(return_value=self.precice_dt)
         Interface.initialize_data = MagicMock()
@@ -203,15 +184,10 @@ class TestWriteData(TestCase):
         precice.initialize(RightBoundary(), self.mesh, read_u, write_u, u_init)
         precice.advance(write_u, u_init, u_init, 0, self.fenics_dt, 0)
 
+        Interface.read_block_scalar_data.assert_not_called()
+
         expected_data_id = 15
-        expected_size = 11
-        expected_values = np.array([i for i in range(2 * 11)])
-        expected_ids = np.zeros(11)
+        expected_ids = self.dummy_vertex_ids
 
-        expected_args = [expected_data_id, expected_size, expected_ids, expected_values]
-
-        for arg, expected_arg in zip(Interface.read_block_vector_data.call_args[0], expected_args):
-            if type(arg) is int:
-                self.assertTrue(arg == expected_arg)
-            elif type(arg) is np.ndarray:
-                np.testing.assert_allclose(arg, expected_arg)
+        self.assertTrue(expected_data_id == Interface.read_block_vector_data.call_args[0][0])
+        np.testing.assert_allclose(expected_ids, Interface.read_block_vector_data.call_args[0][1])
