aa4c98d732c3107432ddbf3363859246bd4dd018
BenjaminRueth <benjamin.rueth@tum.de>
2019-11-27 13:03:33 +0100

    require precice>=2.0.0 and set version of adapter to 1.0.0, since this is a breaking change

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ if sys.version_info[0] < 3:
     raise Exception("The fenics-adapter only supports Python3. Did you run $python setup.py <option>.? Try running $python3 setup.py <option>.")
 
 setup(name='fenics-adapter',
-      version='0.1',
+      version='1.0.0',
       description='preCICE-adapter for the open source computing platform FEniCS ',
       url='https://github.com/precice/fenics-adapter',
       author="Benjamin Rueth",
       author_email='benjamin.rueth@tum.de',
       license='LGPL-3.0',
       packages=['fenicsadapter'],
-      install_requires=['precice_future', 'fenics', 'scipy', 'numpy>=1.13.3'],
+      install_requires=['precice>=2.0.0', 'fenics', 'scipy', 'numpy>=1.13.3'],
       test_suite='tests',
       zip_safe=False)
