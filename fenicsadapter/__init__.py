94e6ab348c2082e60b260bca3df8ca0b37208f20
BenjaminRueth <benjamin.rueth@tum.de>
2019-12-18 13:42:57 +0100

    Revert "filter somewhere else"
    This reverts commit 8d351e2a10cfac71a8f7e28afcd3d126e9182c06.

--- fenicsadapter/__init__.py
+++ fenicsadapter/__init__.py
@@ -1,2 +1,5 @@
 from .fenicsadapter import Adapter, GeneralInterpolationExpression, ExactInterpolationExpression
 
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
