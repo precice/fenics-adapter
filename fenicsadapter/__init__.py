8d351e2a10cfac71a8f7e28afcd3d126e9182c06
BenjaminRueth <benjamin.rueth@tum.de>
2019-12-18 13:21:16 +0100

    filter somewhere else

--- fenicsadapter/__init__.py
+++ fenicsadapter/__init__.py
@@ -1,5 +1,2 @@
 from .fenicsadapter import Adapter, GeneralInterpolationExpression, ExactInterpolationExpression
 
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
