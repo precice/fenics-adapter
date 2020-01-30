00f3962e2b06b45f91366a301c7ae09670f8c610
BenjaminRueth <benjamin.rueth@tum.de>
2020-01-30 21:49:31 +0100

    Require pyprecice version 0.1.0

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(name='fenicsadapter',
       author_email='benjamin.rueth@tum.de',
       license='LGPL-3.0',
       packages=['fenicsadapter'],
-      install_requires=['precice>=2.0.0', 'fenics', 'scipy', 'numpy>=1.13.3'],
+      install_requires=['pyprecice>=0.1.0', 'fenics', 'scipy', 'numpy>=1.13.3'],
       test_suite='tests',
       zip_safe=False)
