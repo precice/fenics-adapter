959fca17b24428dfe1f6156f7d16234335b59305
Benjamin Rüth <benjamin.rueth@tum.de>
2020-11-30 13:14:27 +0100

    Use versioneer for __version__ (#106)
    * Update setup.cfg
    * Add test.

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
 import os
 from setuptools import setup
+import versioneer
 
 # from https://stackoverflow.com/a/9079062
 import sys  
 if sys.version_info[0] < 3:
     raise Exception("fenicsprecice only supports Python3. Did you run $python setup.py <option>.? Try running $python3 setup.py <option>.")
 
-# using point 3. in https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
-version = {}
-with open("fenicsprecice/version.py") as fp:
-    exec(fp.read(), version)
-    
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='fenicsprecice',
-      version=version['__version__'],
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='FEniCS-preCICE adapter is a preCICE adapter for the open source computing platform FEniCS.',
       long_description=long_description,
       long_description_content_type='text/markdown',
