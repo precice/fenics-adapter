c5ece4af5389ac80d8e805daf8780326790d6dbe
BenjaminRodenberg <benjamin.rodenberg@in.tum.de>
2021-04-21 10:33:24 +0200

    Check for FEniCS at runtime and warn, if not found.

--- fenicsprecice/__init__.py
+++ fenicsprecice/__init__.py
@@ -1,3 +1,11 @@
+import warnings
+
+try:
+    from fenics import *
+except ModuleNotFoundError:
+    warnings.warn("No FEniCS installation found on system. Please check whether it is found correctly. "
+            "The FEniCS adapter might not work as expected.\n\n")
+
 from .fenicsprecice import Adapter
 from ._version import get_versions
 __version__ = get_versions()['version']
