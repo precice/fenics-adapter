f24fb23b215a902dbb51a43194c30d929edd679e
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-03-13 14:03:18 +0100

    Some cleaning up and simplification.

--- fenicsprecice/solverstate.py
+++ fenicsprecice/solverstate.py
@@ -1,40 +1,43 @@
 class SolverState:
-    def __init__(self, u, t, n):
+    def __init__(self, payload, t, n):
         """
-        Solver state consists of a value u, associated time t and the timestep n
+        Solver state consists of a payload (either a single fenics.Function or a list of several fenics.Functions), associated time t and the timestep n
 
         Parameters
         ----------
-        #TODO rename u to payload or similar
-        u : Object of class dolfin.functions.function.Function
-            FEniCS function related to the field during each coupling iteration.
-        t : double
-            Time stamp.
-        n : int
-            Iteration number.
-        """
-        self.u = u
-        self.t = t
-        self.n = n
-
-    def get_state(self):
-        """
-        Returns the state variables value u, associated time t and timestep n
-
-        Returns
-        -------
-        u : Object of class dolfin.functions.function.Function
-            A copy of FEniCS function related to the field during each coupling iteration.
+        payload : A fenics.Function or a list of fenics.Functions
+            Describes the state of the solver.
         t : double
             Time stamp.
         n : int
             Iteration number.
         """
         try:
-            return self.u.copy(), self.t, self.n
+            self.payload = payload.copy()
         except AttributeError:  # if .copy() does not exist, it probably is a list
-            return [item.copy() for item in self.u], self.t, self.n
+            self.payload = [item.copy() for item in payload]
+
+        self.t = t
+        self.n = n
+
+    def get_state(self):
+        """
+        Returns the state variables payload, associated time t and timestep n
+
+        Returns
+        -------
+        payload : A fenics.Function or a list of fenics.Functions
+            Describes the state of the solver.
+        t : double
+            Time stamp.
+        n : int
+            Iteration number.
+        """
+        try:
+            return self.payload.copy(), self.t, self.n
+        except AttributeError:  # if .copy() does not exist, it probably is a list
+            return [item.copy() for item in self.payload], self.t, self.n
 
     def print_state(self):
-        u, t, n = self.get_state()
-        return print("u={u}, t={t}, n={n}".format(u=u, t=t, n=n))
+        payload, t, n = self.get_state()
+        return print(f"payload={payload}, t={t}, n={n}")
