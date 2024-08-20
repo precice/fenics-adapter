b04e6c9c628c92a5f97c62e1e9ed676206f05edf
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-08-20 10:43:46 +0200

    Fix autopep8.

--- tests/unit/test_checkpointing.py
+++ tests/unit/test_checkpointing.py
@@ -11,7 +11,7 @@ class TestCheckpointing(TestCase):
         """
         n = 1
         size = 5
-        mesh = UnitSquareMesh(size,size)
+        mesh = UnitSquareMesh(size, size)
         V = FunctionSpace(mesh, 'P', 2)
         dummy_value = 1
         E = Expression("t", t=dummy_value, degree=2)
@@ -22,24 +22,24 @@ class TestCheckpointing(TestCase):
         # "read checkpoint"
         u_cp, t_cp, n_cp = sstate.get_state()
 
-        #check values
+        # check values
         self.assertEqual(t_cp, dummy_value)
         self.assertEqual(n, n_cp)
-        #function should be the same everywhere (-> check vector values of the function)
+        # function should be the same everywhere (-> check vector values of the function)
         vec_u = u.vector()
         vec_u_cp = u_cp.vector()
-        for i in range(size*size):
+        for i in range(size * size):
             self.assertAlmostEqual(vec_u[i], vec_u_cp[i])
 
     def test_solverstate_modification_vector(self):
         """
-        Check if correct values are read from the checkpoint, if the dof vector of the dolfin functions are changed directly 
+        Check if correct values are read from the checkpoint, if the dof vector of the dolfin functions are changed directly
 
         Motivation for this test: Related to https://github.com/precice/fenics-adapter/pull/172 and https://github.com/precice/tutorials/pull/554
         """
         n = 1
         size = 5
-        mesh = UnitSquareMesh(size,size)
+        mesh = UnitSquareMesh(size, size)
         V = FunctionSpace(mesh, 'P', 2)
         ref_value = 1
         E = Expression("t", t=ref_value, degree=2)
@@ -55,13 +55,12 @@ class TestCheckpointing(TestCase):
         # "read checkpoint"
         u_cp, _, _ = sstate.get_state()
 
-        #check values
-        #function should be the same everywhere
-        #(so the vector values should all be ref_value)
+        # check values
+        # function should be the same everywhere
+        # (so the vector values should all be ref_value)
         vec_u_cp = u_cp.vector()
-        for i in range(size*size):
+        for i in range(size * size):
             self.assertAlmostEqual(ref_value, vec_u_cp[i])
-    
 
     def test_solverstate_modification_assign(self):
         """
@@ -70,7 +69,7 @@ class TestCheckpointing(TestCase):
         """
         n = 1
         size = 5
-        mesh = UnitSquareMesh(size,size)
+        mesh = UnitSquareMesh(size, size)
         V = FunctionSpace(mesh, 'P', 2)
         ref_value = 1
         E = Expression("t", t=ref_value, degree=2)
@@ -83,15 +82,15 @@ class TestCheckpointing(TestCase):
         # "compute" new solution
         dummy_value = ref_value + 2
         E.t = dummy_value
-        u2 = interpolate(E,V)
+        u2 = interpolate(E, V)
         u.assign(u2)
 
         # "read checkpoint"
         u_cp, _, _ = sstate.get_state()
 
-        #check values
-        #function should be the same everywhere
-        #(so the vector values should all be ref_value)
+        # check values
+        # function should be the same everywhere
+        # (so the vector values should all be ref_value)
         vec_u_cp = u_cp.vector()
-        for i in range(size*size):
+        for i in range(size * size):
             self.assertAlmostEqual(ref_value, vec_u_cp[i])
