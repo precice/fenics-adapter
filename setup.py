4ed1a1a79971ccc15830587ee9e3e6908e938ad7
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-09-05 11:38:32 +0200

    Update setup.py
    Co-authored-by: Frédéric Simonis <simonisfrederic@gmail.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
 from setuptools import setup
 import versioneer
-import warnings
-
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
