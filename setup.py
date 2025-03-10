ec0a9f387f1c739b9ce3c5515cef15e138d5533f
giuliatum <202611957+giuliatum@users.noreply.github.com>
2025-03-10 12:50:00 +0100

    Remove preCICE as dependency for easier development.

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='fenicsprecice',
       author_email='info@precice.org',
       license='LGPL-3.0',
       packages=['fenicsprecice'],
-      install_requires=['pyprecice>=3.0.0.0', 'scipy<1.15.0', 'numpy>=1.13.3, <2', 'mpi4py<4'],
+      install_requires=['scipy<1.15.0', 'numpy>=1.13.3, <2', 'mpi4py<4'],
       test_suite='tests',
       zip_safe=False)
