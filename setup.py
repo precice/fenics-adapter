f8b34f05d9ad7a3bfd7479a754273dc9900ca471
Ishaan Desai <ishaandesai@gmail.com>
2026-02-11 10:40:03 +0100

    Fix mpi4py to 4.0.0 because <4 no longer works with modern Python

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='fenicsprecice',
           'pyprecice>=3.0.0.0',
           'scipy<1.15.0',
           'numpy>=1.13.3, <2',
-          "mpi4py<4",
+          "mpi4py==4.0.0",
       ],
       test_suite='tests',
       zip_safe=False)
